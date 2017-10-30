e8043eef1c169359fe46c91972d464bf8e1b6a3c
David Petringa <david@ajaxtransport.com>
2017-10-30 13:32:53 -0400

    cleaned up code

--- resources/views/menu/show.blade.php
+++ resources/views/menu/show.blade.php
@@ -2,17 +2,18 @@
 {{-- show all the dishes link to full dish description--}}
 @extends('layouts.master')
 
+
 @section('title')
     All Dishes
 @endsection
 
 @section('content')
-    <div class="content">
-	    <h1>All Dishes</h1>
-	        <ul>
-                @foreach($dishes as $key => $dish)
-	               <li><a href='find-dish/{{ str_replace(' ', '-', $key) }}'>{{ $key }}</a></li>
-	            @endforeach
-            </ul>
+    <div class='content'>
+	    <h2>All Dishes</h2>
+	    <ul class='outputDisplay'>
+            @foreach($dishes as $key => $dish)
+	            <li><a href='find-dish/{{ str_replace(' ', '-', $key) }}'>{{ $key }}</a></li>
+            @endforeach
+        </ul>
     </div>
 @endsection
