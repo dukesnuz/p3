e8f1aae530df37d3c382b67e9633b63c233d8d9f
David Petringa <david@ajaxtransport.com>
2018-12-05 11:58:12 -0500

    added stat counter

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>
-	    @yield('title')
+		@yield('title')
 	</title>
 
 	<link href="/css/p3.css?t=<?php echo rand(); ?>" rel="stylesheet" type="text/css">
@@ -37,7 +37,7 @@
 
 		<section>
 			@yield('content')
-        </section>
+		</section>
 
 		<footer>
 			@include('footers.footermain')
@@ -46,6 +46,21 @@
 	</div><!--END wrapper div-->
 
 	@stack('body')
-
-</body>
-</html>
+	<!-- Default Statcounter code for Harvard Extension DWA
+	http://http//www.dukesnuz.com -->
+	<script type="text/javascript">
+	var sc_project=11889370;
+	var sc_invisible=1;
+	var sc_security="6ab59e53";
+	</script>
+	<script type="text/javascript"
+	src="https://www.statcounter.com/counter/counter.js"
+	async></script>
+	<noscript><div class="statcounter"><a title="Web Analytics"
+		href="http://statcounter.com/" target="_blank"><img
+		class="statcounter"
+		src="//c.statcounter.com/11889370/0/6ab59e53/1/" alt="Web
+		Analytics"></a></div></noscript>
+		<!-- End of Statcounter Code -->
+	</body>
+	</html>
