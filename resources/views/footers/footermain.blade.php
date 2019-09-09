ff9da29f0547056c3636f3431e69cf87b068921e
David Petringa <david@ajaxtransport.com>
2019-09-09 13:25:21 -0400

    added link in footer to dukesnuz

--- resources/views/footers/footermain.blade.php
+++ resources/views/footers/footermain.blade.php
@@ -3,7 +3,8 @@
 	    <li>School: Harvard Extension</li>
 	    <li>Class: Dynamic Web Applications</li>
 	    <li>Assignment: Project three</li>
-	    <li>Student: David Petringa</li>
+      <li><a href="http://dukesnuz.com/self-study-courses/courses-menu/dukesnuz-david-petringa#e-15">
+        Student: David Petringa</a></li>
 	    <li>Coded: September 2017</li>
 	    <li>Contact me: {{ config('mail.supportEmail') }}</li>
     </ul>
